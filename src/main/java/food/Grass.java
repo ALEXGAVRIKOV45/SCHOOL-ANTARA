f0c7d9c91f167c6883f251c1fbec53c0b5a23632
Алексей Гавриков <85308969+ALEXGAVRIKOV45@users.noreply.github.com>
2022-05-10 22:07:16 +0500

    clean

--- src/main/java/food/Grass.java
+++
@@ -1,7 +0,0 @@
-package food;
-// Растительная еда
-public class Grass extends Food{
-	public Grass(String name, int nutritional) {
-		super(name,nutritional);
-	}
-}
--- src/main/java/food/Meat.java
+++
@@ -1,7 +0,0 @@
-package food;
-// Мясная еда
-public class Meat extends Food{
-	public Meat(String name, int nutritional) {
-		super(name,nutritional);
-	}
-}
